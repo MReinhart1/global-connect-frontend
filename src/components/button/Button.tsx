407b35de4f0932a2e27e42102f3364368457dbff
vovka26 <317110@gmail.com>
2024-01-16 17:49:22 -0500

    update disabled hover btn color

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,7 +4,7 @@ import { Button as ButtonAnt } from 'antd'
 const Button = (props?: ButtonProps) => {
   return (
     <ButtonAnt
-      className="flex w-full justify-center rounded-md bg-blue-600 px-3 text-white shadow-sm hover:bg-blue-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex w-full justify-center rounded-md bg-blue-600 px-3 text-white shadow-sm hover:bg-blue-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-60 disabled:hover:bg-gray-100 disabled:hover:text-gray-400"
       {...props}
     />
   )
